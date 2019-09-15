dc13b1a85e5dc0d9efb3665856eb9338e1845573
Anders Högqvist <andho@localhost>
2019-09-15 19:20:25 +0200

    Performance fix
    * Added a control statement to all arithmetic functions so that they're
    performed bit-by-bit only if Size > 64. That would improve performance
    quite nice.
    * Also added all the comparison operators to the Int class so that they
    work for negative numbers.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # custom_int
 An integer (signed and unsigned) class based on std::bitset
 
-# Background
+### Background
 During a project at my work I needed a convenient way to store values taking up the smallest amount of bits necessary, not in memory but later when they're serialized into an array of unsigned short ints. For instance, a value that could be between 0 and 7 was only supposed to be 3 bits long. I ended up developing my own solution based on std::bitset with some additional code in order to make it work with signed values. Another goal was to make it work as close to a regular integer type as possible so I also included arithmetics.
 
-# Usage
+### Usage
 The idea was to make the usage as close to a regular integral type as possible.
 
 #### Initialization:
@@ -26,7 +26,8 @@ Uint9 value4;
 ```
 
 #### Arithmetics
-I've implemented support for just about every kind arithmetic operations including additon, subtraction, division and multiplication.
+I've implemented support for just about every kind arithmetic operations including additon, subtraction, division and multiplication. For instantiation with > 64 bits these arithmetics are done bit-by-bit with a quite large performance hit as a result.
+
 ```
 Int<5> value1, value2;
 
@@ -42,5 +43,10 @@ value1 * -2;
 
 etc...
 ```
-# Contribution
+
+### Contribution
 I welcome any idea on how to improve this and please let me know if you find anything that isn't working!
+
+
+### Todo
+I've not implemented a bit-by-bit implementation of division because I couldn't figure out how to do it.
--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <limits>
 
 #include "custom_int.h"
 
 using namespace std;
 
+typedef Int<13> Int13;
+typedef UInt<5> Uint5;
+
 int main() {
-  Int<13> test1 = 1;
-  UInt<5> test2;
+  Int13 test1 = 1;
+  Uint5 test2;
+  Int<64> large_signed;
+  UInt<64> large_unsigned;
 
   cout << "test1: " << test1 << endl;
   cout << "test2: " << test2 << endl;
@@ -35,4 +41,43 @@ int main() {
   test2 = test2 / 10;
 
   cout << "test2: " << test2 << endl;
+
+  large_signed = numeric_limits<long long>::max();
+
+  cout << "large_signed: " << large_signed << endl;
+
+  large_signed = numeric_limits<long long>::min();
+
+  cout << "large_signed: " << large_signed << endl;
+
+  large_unsigned = numeric_limits<unsigned long long>::max();
+
+  cout << "large_unsigned: " << large_unsigned << endl;
+
+  large_unsigned -= 100000;
+
+  cout << "large_unsigned: " << large_unsigned << endl;
+
+  auto another_large = large_signed;
+
+  another_large -= 100000;
+
+  cout << large_signed << " < " << another_large << "? ";
+  large_signed < another_large ? (cout << "Yes" << endl) : (cout << "No" << endl);
+
+  another_large = large_signed;
+
+  cout << "another_large: " << another_large << endl;
+
+  cout << large_signed << " <= " << another_large << "? ";
+  large_signed <= another_large ? (cout << "Yes" << endl) : (cout << "No" << endl);
+
+  cout << large_signed << " < " << another_large << "? ";
+  large_signed < another_large ? (cout << "Yes" << endl) : (cout << "No" << endl);
+
+  cout << large_signed << " > " << another_large << "? ";
+  large_signed > another_large ? (cout << "Yes" << endl) : (cout << "No" << endl);
+
+  cout << large_signed << " >= " << another_large << "? ";
+  large_signed >= another_large ? (cout << "Yes" << endl) : (cout << "No" << endl);
 }
